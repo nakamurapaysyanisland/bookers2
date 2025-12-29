f5f3170e7c9b587c26d1d25e94f85a2255855a5a
YukaNakamura <242622452+nakamurapaysyanisland@users.noreply.github.com>
2025-12-29 06:04:51 +0000

    [Finish]user.editページ完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,4 +23,18 @@ class UsersController < ApplicationController
         @user = User.save
         redirect_to user_path
     end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to user_path(@user)
+        else
+            render :edit
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :introduction, :profile_image)
+    end
 end
