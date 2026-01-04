cc800b604fedadf067a969ae6f718bdda5c16b89
YukaNakamura <242622452+nakamurapaysyanisland@users.noreply.github.com>
2026-01-04 02:18:32 +0000

    [Finish]

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get "home/about" => "homes#about", as: "about"
   root to: "homes#top"
-
   resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only:[:show, :edit, :index, :update]
 end
